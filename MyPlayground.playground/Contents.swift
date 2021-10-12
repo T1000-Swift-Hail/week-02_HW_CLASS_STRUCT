cae078d6a2038c030f9a3a8bcc7f15ea2006e07f
ahmad1aa <Ahmed.Atallah.has.s@tuwaiq.edu.sa>
2021-10-12 23:24:54 +0300

    hw done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -47,3 +47,35 @@ var PlayersRMD : Player = Player(name: "pepe", height: 1.87, location: "center o
  RealMadrid.players = [PlayersRMM,PlayersRMR,PlayersRMK,PlayersRMD]
  
  RealMadrid.describeTeam()
+
+
+var Playerslps : Player = Player(name: "mohammad", height: 1.70, location: "weng", skillLevel: "skill", shirtNumber: 12, position: "right weng")
+
+var Playerslpm : Player = Player(name: "mani", height: 1.69, location: "weng", skillLevel: "speed", shirtNumber: 4, position: "left weng")
+
+var Playerslpf : Player = Player(name: "firminio", height: 1.78, location: "striker", skillLevel: "speed", shirtNumber: 2, position: "atack")
+
+var Playerslpg : Player = Player(name: "gerard", height: 1.85, location: "center ", skillLevel: "carer", shirtNumber: 3, position: "middle")
+
+var Leverpool:Team=Team()
+Leverpool.name = ""
+Leverpool.players = [Playerslps,Playerslpm,Playerslpf,Playerslpg]
+
+Leverpool.describeTeam()
+
+
+var Playersals : Player = Player(name: "sami", height: 1.75, location: "striker", skillLevel: "skill", shirtNumber: 9, position: "atack")
+
+var Playersalm : Player = Player(name: "alshlhob", height: 1.65, location: "middle", skillLevel: "legend", shirtNumber: 10, position: "hart of team and mine")
+
+var Playersaly : Player = Player(name: "yosef", height: 1.78, location: "weng", skillLevel: "skill", shirtNumber: 15, position: "right weng")
+
+var Playersal : Player = Player(name: "gomes", height: 1.87, location: "center of atack", skillLevel: "strong", shirtNumber: 18, position: "atack")
+
+var Alhlal:Team=Team()
+Alhlal.name = ""
+Alhlal.players = [PlayersRMM,PlayersRMR,PlayersRMK,PlayersRMD]
+
+Alhlal.describeTeam()
+
+
