5d222d32d02d04a3abc00558d040217d96c3669e
hanaalmutlak <Hana.Khamis.has.s@tuwaiq.edu.sa>
2021-10-13 11:11:42 +0300

    solve

--- week-02_HW_CLASS_STRUCT.playground/Contents.swift
+++ week-02_HW_CLASS_STRUCT.playground/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 
-struct City {
+struct Player {
 
 var name :String
     
@@ -17,8 +17,7 @@ var position : String
 
     
     
-    func describePlayer(name:String, height : Int ,location : String ,skillLevel : String ,shirtNumber : String
-, position : String) {
+    func describePlayer() {
         print("Player\(location) name is \(name) , he is \(height) meters tall. His skill level is \(skillLevel) and playes in position \(position) . His T-Shirt number is \(shirtNumber).")
         
 }
@@ -28,27 +27,26 @@ class Team {
     
     var name : String = ""
     
-    var Players : [player] = []
+    var player : [Player] = []
     
     func describeTeam () {
         
-    for Playerss in Players {
+    for players in player {
             
-        Players.describePlayer(name: Player.name , height: Player.height , location: Player.location , skillLevel: Player.skillLevel , shirtNumber: Player.shirtNumber ,position: player.position )
+        players.describePlayer()
         }
     }
 }
-let firstplayer = player(name: "Ronaldo" , height:  1.80 , location: "realMadrid", skillLevel: "very high" , shirtNumber:  1 , position: "defense")
+let firstplayer = Player(name: "Ronaldo" , height:  1.80 , location: "realMadrid", skillLevel: "very high" , shirtNumber:  1 , position: "defense")
    
-    let secplayer = Players(name : "Marcelo" , height : 1.90 , location : "realMadrid", skillLevel : "high" , shirtNumber :  20 , position : "guard")
-    let thirplayer =  Players (name : "Nacho" , height : 1.75 , location : "realMadrid" , skillLevel : "very high" , shirtNumber :  6 , position : "Midfielder")
-    let forplayer = Players (name: "Raphael Varane" , height :  1.70 , location : "realMadrid" , skillLevel : "high" , shirtNumber :  10 , position : "defense")
-
+    let secplayer = Player(name : "Marcelo" , height : 1.90 , location : "realMadrid", skillLevel : "high" , shirtNumber :  20 , position : "guard")
+    let thirplayer =  Player(name : "Nacho" , height : 1.75 , location : "realMadrid" , skillLevel : "very high" , shirtNumber :  6 , position : "Midfielder")
+    let forplayer = Player(name: "Raphael Varane" , height :  1.70 , location : "realMadrid" , skillLevel : "high" , shirtNumber :  10 , position : "defense")
 
 let TeamPly = Team()
     TeamPly.name = "RealMadrid"
     print(TeamPly.name)
-TeamPly.Players.append(contentsof : [firstplayer,secplayer,thirplayer,forplayer])
+TeamPly.player.append(contentsOf:[firstplayer,secplayer,thirplayer,forplayer])
 TeamPly.describeTeam()
 print(TeamPly)
 
